e8a37d0800e6360d993ab3dc5c9f000678a9ae9c
aromjim <aromjim@gmail.com>
2017-01-25 23:28:23 +0100

    Move package installation configuration to init.el
    For a correct functioning of installed packages they must be initialized after setting the
    installation directory.

--- conf-available.d/package.el
+++
@@ -1,8 +0,0 @@
-;;; Package installation configuration
-
-(use-package package
-  :bind ("C-c p" . list-packages)
-  :config
-  (add-to-list 'package-archives
-	       '("melpa" . "https://melpa.org/packages/"))
-  (setq package-user-dir "/usr/local/share/emacs/site-lisp"))
