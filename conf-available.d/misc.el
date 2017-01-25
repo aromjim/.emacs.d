b51a8de73f9bfccd61eaf7ec7adb61f4851068de
aromjim <aromjim@gmail.com>
2017-01-25 18:19:27 +0100

    Set Emacs behaviour to personal taste

--- conf-available.d/misc.el
+++ conf-available.d/misc.el
@@ -16,3 +16,30 @@
 
 ;; Disable toolbar
 (tool-bar-mode -1)
+
+
+;;; Miscellaneous configurations
+
+;; Column number in mode line
+(column-number-mode t)
+
+;; Default value for automatic line-wrapping column
+(setq-default fill-column 99)
+
+;; Visualize matching parens
+(show-paren-mode t)
+
+;; Avoid cursor and mouse pointer clash
+(mouse-avoidance-mode 'animate)
+
+;; Save minibuffer history
+(savehist-mode)
+
+;; Activate Auto Fill in Text mode and related modes
+(toggle-text-mode-auto-fill)
+
+;; A single space does end a sentence
+(setq sentence-end-double-space nil)
+
+;; Don't make a backup of a file the first time it is saved
+(setq make-backup-files nil)
