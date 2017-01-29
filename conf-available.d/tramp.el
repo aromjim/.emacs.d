e8795cf90ffe154445f7fc2ccdfab8bb98e37cb8
aromjim <aromjim@gmail.com>
2017-01-29 22:10:28 +0100

    Refactor TRAMP configuration to use read-server-file

--- conf-available.d/tramp.el
+++ conf-available.d/tramp.el
@@ -26,14 +26,10 @@
 (use-package tramp
   :defer t
   :config
-  (let* ((add-server (lambda (server username &optional port) ; Add hop for sudoing into server
-		       (add-to-list 'tramp-default-proxies-alist
-				    (list server
-					  "root"
-					  (s-lex-format "/ssh:${username}@${server}:")))))
-	 (filename (locate-user-emacs-file servers-file))
-	 (servers (f-read-text filename 'utf-8)))
-    (mapc (lambda (server)
-	    (apply add-server (s-split "[[:space:]]+" server)))
-	  (s-split "\n" servers t))))
-
+  (let ((servers (read-server-file)))
+    (--each servers (let ((server (car it))
+			  (username (cadr it)))
+		      (add-to-list 'tramp-default-proxies-alist
+				   (list server
+					 "root"
+					 (s-lex-format "/ssh:${username}@${server}:")))))))
