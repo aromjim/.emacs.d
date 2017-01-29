a091213a32e04e70b32296dd7a8393978c03b4bf
aromjim <aromjim@gmail.com>
2017-01-29 13:58:27 +0100

    Add command to knock servers
    Port knocking is a method of externally opening ports on a firewall by generating a connection
    attempt on a set of prespecified closed ports.

--- conf-available.d/tramp.el
+++ conf-available.d/tramp.el
@@ -1,13 +1,32 @@
+;; User emacs file to save server data
+(defvar server-file "servers.private")
+
+(defun read-server-file (&optional with-port-only)
+  (let* ((filename (locate-user-emacs-file server-file))
+	 (server-list (f-read-text filename 'utf-8))
+	 (servers (--map (s-split "[[:space:]]+" it)
+			 (s-split "\n" server-list t))))
+    (if with-port-only
+	(--filter (= (length it) 3) servers)
+      servers)))
+
+(defun knock-remote-server ()
+  (interactive)
+  (let* ((servers (read-server-file t))
+	 (server-names (-select-column 0 servers))
+	 (server (completing-read (concat "Server to knock (default "
+					  (car server-names)
+					  "): ")
+				  server-names nil t nil nil server-names))
+	 (port (cadr (alist-get server servers))))
+    (call-process "knock" nil nil nil server port)))
+
+(global-set-key (kbd "C-c k") 'knock-remote-server)
+
 (use-package tramp
   :defer t
   :config
-  (use-package f)
-  (use-package s)
-
-  ;; User emacs file to save server data
-  (defvar servers-file "servers.private")
-  
-  (let* ((add-server (lambda (server username) ; Add hop for sudoing into server
+  (let* ((add-server (lambda (server username &optional port) ; Add hop for sudoing into server
 		       (add-to-list 'tramp-default-proxies-alist
 				    (list server
 					  "root"
@@ -17,3 +36,4 @@
     (mapc (lambda (server)
 	    (apply add-server (s-split "[[:space:]]+" server)))
 	  (s-split "\n" servers t))))
+
