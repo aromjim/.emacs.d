3a281917032c9c5bbfd41537abe077eb579a117a
aromjim <aromjim@gmail.com>
2017-01-30 15:53:52 +0100

    Activate several electric behaviours

--- conf-available.d/auctex.el
+++ conf-available.d/auctex.el
@@ -8,4 +8,16 @@
 
   ;; Activate RefTeX mode
   (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-  (setq reftex-plug-into-AUCTeX t))
+  (setq reftex-plug-into-AUCTeX t)
+
+  ;; Bound character '\' to TeX-electric-macro
+  (setq TeX-electric-escape t)
+
+  ;; Automatically insert the opening and closing symbols for an inline equation
+  (setq TeX-electric-math t)
+
+  ;; Automatically insert braces for sub and superscripts in math mode
+  (setq TeX-electric-sub-and-superscript t)
+
+  ;; Automatically insert the corresponding closing brace when writing an opening brace
+  (setq LaTeX-electric-left-right-brace t))
