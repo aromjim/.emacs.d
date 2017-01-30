2d6f0b6d0b4e8d116c3c68cb8add84629f438f47
aromjim <aromjim@gmail.com>
2017-01-30 15:31:42 +0100

    Activate Flyspell and RefTeX modes for LaTeX documents

--- conf-available.d/auctex.el
+++ conf-available.d/auctex.el
@@ -1,4 +1,11 @@
 ;;; Configuration of AUCTeX, a package for writing (La)TeX files
 
 (use-package tex
-  :defer t)
+  :defer t
+  :config
+  ;; Activate on the fly spell checking
+  (add-hook 'LaTeX-mode-hook 'turn-on-flyspell)
+
+  ;; Activate RefTeX mode
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+  (setq reftex-plug-into-AUCTeX t))
