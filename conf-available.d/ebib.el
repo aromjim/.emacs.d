3e73bed30a42b703c9044bfd6f09a43c9551cd76
aromjim <aromjim@gmail.com>
2017-02-03 20:46:44 +0100

    Customize Ebib hidden and extra fields

--- conf-available.d/ebib.el
+++ conf-available.d/ebib.el
@@ -13,4 +13,12 @@
 	bibtex-autokey-year-length 0
 	bibtex-autokey-titleword-length 'infty
 	bibtex-autokey-year-title-separator ":")
+  ;; Customization of hidden fields
+  (--each '("hyphenation" "year") (add-to-list 'ebib-hidden-fields it))
+  (--each '("chapter" "edition" "isbn" "issn" "language" "pagetotal")
+    (delete it ebib-hidden-fields))
+  ;; Customization of extra fields
+  (let ((fields (assq 'biblatex ebib-extra-fields)))
+    (setcdr fields (append '("langid" "langidopts")
+			   (cdr fields))))
   )
