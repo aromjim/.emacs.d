f2b4f40a546e28e4fbb4e27a86783473b98aed54
aromjim <aromjim@gmail.com>
2017-02-03 20:12:08 +0100

    Specify bibliographic databases to preload on startup

--- conf-available.d/ebib.el
+++ conf-available.d/ebib.el
@@ -1,2 +1,8 @@
 (use-package ebib
-  :bind ("C-c b" . ebib))
+  :bind ("C-c b" . ebib)
+  :config
+  ;; Directories containing bibliographic databases
+  (setq ebib-bib-search-dirs '("~/.texmf/bibtex/bib/personal"))
+  ;; Bibliographic databases to load on startup
+  (setq ebib-preload-bib-files '("Bibliography.bib"))
+  )
