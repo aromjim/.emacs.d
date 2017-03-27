7e68e90fb1a57b71173ef34c52329a57155552e9
aromjim <aromjim@gmail.com>
2017-03-27 13:51:08 +0200

    Allow recursive copies and deletes in dired buffers

--- conf.d/dired.el
+++ conf.d/dired.el
@@ -1,3 +1,9 @@
 ;; Human-readable format for file sizes
 (setq list-directory-verbose-switches "-lh")
 (setq dired-listing-switches "-alh")
+
+;; Recursive copies and deletes allowed
+;; but move deleted files and directories into trash
+(setq dired-recursive-copies 'always)
+(setq dired-recursive-deletes 'always)
+(setq delete-by-moving-to-trash t)
