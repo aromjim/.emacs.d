60398d5756b6b2588d71b208496232f3693e7167
aromjim <aromjim@gmail.com>
2017-09-08 12:07:02 +0200

    Set key bindings for Counsel version of commands

---
+++ conf.d/counsel.el
@@ -0,0 +1,13 @@
+(use-package counsel
+  :config
+  ;; Global key bindings
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "<menu>") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "C-x l") 'counsel-locate)
+  (global-set-key (kbd "C-c m") 'counsel-bookmark)
+  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  (global-set-key (kbd "<f2> u") 'counsel-unicode-char))
