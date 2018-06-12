1003c1435e899b981d53b0f55a0feb5ebe5f956d
Álvaro Romero-Jiménez <aromjim@gmail.com>
2018-06-12 12:13:30 +0200

    Install Magit package

---
+++ conf.d/magit.el
@@ -0,0 +1,11 @@
+;;; Configuration of Magit, an interface to the version control system Git
+
+(use-package magit
+  :straight t
+  :bind ("C-c g" . magit-status)
+  :config
+  ;; Disable Emacs default version control interface
+  (setq vc-handled-backends nil)
+  ;; Use the entire frame when displaying a status buffer
+  (setq magit-display-buffer-function
+	#'magit-display-buffer-fullframe-status-v1))
