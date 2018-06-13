831eb41bcf1cfa617134beb293c631f091870f97
Álvaro Romero-Jiménez <aromjim@gmail.com>
2018-06-13 18:11:33 +0200

    Defer loading of PDF Tools package by means of :magic keyword

--- conf.d/pdf-tools.el
+++ conf.d/pdf-tools.el
@@ -1,8 +1,7 @@
 (use-package pdf-tools
   :straight t
-  :defer t
-  :init
-  (pdf-tools-install)
+  :magic ("%PDF" . pdf-view-mode)
   :config
+  (pdf-tools-install)
   ;; Use normal isearch
   (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
