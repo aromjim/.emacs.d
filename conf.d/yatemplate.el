0f063cf9bf6fa33d15dfb15f0e26cb977b0abe3e
Álvaro Romero-Jiménez <aromjim@gmail.com>
2019-01-31 10:43:55 +0100

    Refactor exam template

--- conf.d/yatemplate.el
+++ conf.d/yatemplate.el
@@ -2,7 +2,19 @@
   :straight t
   :defer t
   :init
-  (yatemplate-fill-alist))
+  (yatemplate-fill-alist)
+  :config
+  (setq asignaturas '("Lógica Informática"
+		      "Técnicas de Computación para la Estadística"))
+  (setq titulaciones '("Grado en Estadística"
+		       "Grado en Ingeniería Informática"))
+  (setq convocatorias '("diciembre"
+			"febrero"
+			"junio"
+			"septiembre"))
+  (setq exámenes '("Primer control"
+		   "Segundo control"
+		   "Examen")))
 
 (use-package autoinsert
   :defer t
