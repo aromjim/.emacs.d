77115665b385e39d05beb22ee32cdf2e9db5e8a5
Álvaro Romero-Jiménez <aromjim@gmail.com>
2019-04-04 16:40:26 +0200

    Install YASnippet official snippet collections

--- conf.d/yasnippet.el
+++ conf.d/yasnippet.el
@@ -1,5 +1,9 @@
 (use-package yasnippet
   :straight t
-  :defer t
+  :after yasnippet-snippets
   :init
-  (yas-global-mode 1))
+  (yas-global-mode))
+
+(use-package yasnippet-snippets
+  :straight t
+  :defer t)
