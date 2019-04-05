d27eaec6773a8765799aa8dbfbf824bbec19609a
Álvaro Romero-Jiménez <aromjim@gmail.com>
2019-04-05 19:56:03 +0200

    Define interactive command for inserting current date
    The command allows to choose between different formats using ivy interface.

--- conf.d/utils.el
+++ conf.d/utils.el
@@ -8,7 +8,9 @@
     					; Used in Ebib configuration
     					; Used in TRAMP configuration
     					; Used in RefTeX configuration
-	     --filter)			; Used in TRAMP configuration
+	     --filter			; Used in TRAMP configuration
+	     --map			; Used in insert-date command
+	     )
   )
 
 (use-package f
@@ -20,3 +22,27 @@
   :straight t
   :commands s-lex-format		; Used in TRAMP configuration
   )
+
+;; Command for inserting current date
+(defvar date-formats
+  '("%0A %-e de %B de %Y"		; viernes 5 de abril de 2019
+    "%-e de %B de %Y"			; 5 de abril de 2019
+    "%d-%m-%Y"				; 05-04-2019
+    "%d/%m/%Y"				; 05/04/2019
+    "%F"				; 2019-04-05
+    "%Y/%m/%d"				; 2019/04/05
+    )
+  "Available date formats for insert-date command")
+
+(defun insert-date ()
+  "Insert date at point."
+  (interactive)
+  (ivy-read "Insert date: "
+	    (--map (format-time-string it) date-formats)
+	    :preselect 0
+	    :action (lambda (date)
+		      (when (use-region-p) (delete-region (region-beginning) (region-end)))
+		      (insert date))
+	    :caller 'insert-date))
+
+(global-set-key (kbd "C-c d") 'insert-date)
